fc1380726f483b4288b2d1b128c04a746e65456c
KangPvP <82943439+KangPvP@users.noreply.github.com>
2023-04-09 21:55:23 +0200

    Début du projet Police

---
+++ src/main/java/fr/kanpvp/copsplugin/CopsPlugin.java
@@ -0,0 +1,18 @@
+package fr.kanpvp.copsplugin;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CopsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println(ChatColor.GREEN + "The plugin CopsPlugin is onEnable");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
